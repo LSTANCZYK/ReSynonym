4b97a1205c51723c62f2bb890332dc190ac77f54
HESLS002 <Sam.Heslop@disney.com>
2014-02-12 20:06:20 +0000

    Solution preparation commit and slight refactoring.

--- ReSynonym/AboutAction.cs
+++ ReSynonym/AboutAction.cs
@@ -9,7 +9,6 @@ namespace ReSynonym
     {
         public bool Update(IDataContext context, ActionPresentation presentation, DelegateUpdate nextUpdate)
         {
-            // return true or false to enable/disable this action
             return true;
         }
 
--- ReSynonym/SynonymAction.cs
+++ ReSynonym/SynonymAction.cs
@@ -7,67 +7,42 @@ using JetBrains.DataFlow;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Bulbs;
 using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
-using JetBrains.ReSharper.Feature.Services.LinqTools;
 using JetBrains.ReSharper.Intentions.Extensibility;
-using JetBrains.ReSharper.Psi.CSharp;
-using JetBrains.ReSharper.Psi.CSharp.Tree;
-using JetBrains.ReSharper.Psi.Tree;
-using JetBrains.ReSharper.Refactorings.Rename;
-using JetBrains.ReSharper.Refactorings.Rename.Impl;
 using JetBrains.ReSharper.Refactorings.Workflow;
 using JetBrains.TextControl;
 using JetBrains.Util;
+using ReSynonym.Workflow;
 
 namespace ReSynonym
 {
-    /// <summary>
-    /// This is an example context action. The test project demonstrates tests for
-    /// availability and execution of this action.
-    /// </summary>
-    [ContextAction(Name = "Synonym", Description = "Get synonyms for word", Group = "C#")]
+    [ContextAction(Name = "Synonym", Description = "Suggest Synonyms", Group = "C#")]
     public class SynonymAction : ContextActionBase
     {
-        private readonly ICSharpContextActionDataProvider _provider;
-        private ILiteralExpression _stringLiteral;
+        private readonly ICSharpContextActionDataProvider provider;
+        
 
         public SynonymAction(ICSharpContextActionDataProvider provider)
         {
-            _provider = provider;
-        }
-
-        public override bool IsAvailable(IUserDataHolder cache)
-        {
-            return true;
+            this.provider = provider;
         }
 
         protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
         {
-            //ar renameRefactorService = solution.GetComponent<RenameRefactoringService>();
             var synonyms = new List<string>() {"Sam", "Sam", "Sam"};
-            IRefactoringWorkflow renameRefactoringWorkflow = new InlineRenameWorkflowDecorator(synonyms);
-            //var renameRefactoringWorkflow = new InlineRenameWorkflow("InlineTypoRename");
+
+            IRefactoringWorkflow inlineSynonymWorkflow = new InlineSynonymWorkflow(synonyms);
+
             Lifetimes.Using(lifetime =>
               RefactoringActionUtil.ExecuteRefactoring(
                           JetBrains.ActionManagement.ShellComponentsEx.ActionManager(Shell.Instance.Components)
                             .DataContexts.CreateOnSelection(lifetime, DataRules.AddRule("DoTypoRenameWorkflow",
-                            JetBrains.ProjectModel.DataContext.DataConstants.SOLUTION, solution)), renameRefactoringWorkflow));
+                            JetBrains.ProjectModel.DataContext.DataConstants.SOLUTION, solution)), inlineSynonymWorkflow));
             return null;
-//            CSharpElementFactory factory = CSharpElementFactory.GetInstance(_provider.PsiModule);
-//
-//            var stringValue = _stringLiteral.ConstantValue.Value as string;
-//            if (stringValue == null)
-//                return null;
-//
-//            var chars = stringValue.ToCharArray();
-//            Array.Reverse(chars);
-//            ICSharpExpression newExpr = factory.CreateExpressionAsIs("\"" + new string(chars) + "\"");
-//            _stringLiteral.ReplaceBy(newExpr);
-//            return null;
         }
 
-        public override string Text
-        {
-            get { return "Get Synonym"; }
-        }
+        public override string Text { get { return "Suggest Synonyms"; } }
+
+        public override bool IsAvailable(IUserDataHolder cache) { return true; }
+        
     }
 }
